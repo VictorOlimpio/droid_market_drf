9471d74a280d2125b4b23f2e78af600e7a7ab64f
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-25 13:04:57 -0300

    feature: adiciona autenticação ao usuário customizado

--- users/models.py
+++ users/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy
+from django.conf import settings
 
-class User(models.Model):
-    name = models.CharField(max_length=200)
+class User(AbstractUser):
+    username = models.CharField(blank=True, null=True, max_length=50)
+    email = models.EmailField(ugettext_lazy('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=9)
     ddd = models.CharField(max_length=4)
-    email = models.CharField(max_length=100)
     kind = models.IntegerField(default=0)
