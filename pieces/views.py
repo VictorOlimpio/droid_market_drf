379ea2493c8867b778b8db8720331b5f9d7c4611
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-25 22:18:54 -0300

    feature: adiciona views de pieces

--- pieces/views.py
+++ pieces/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from pieces.models import Piece
+from pieces.serializers import PieceSerializer
+from pieces.permissions import IsLoggedInUserOrAdmin
+from users.permissions import IsAdminUser
 
-# Create your views here.
+class PiecesVewSet(viewsets.ModelViewSet):
+    queryset = Piece.objects.all()
+    serializer_class = PieceSerializer
+
+    def get_permissions(self):
+        permission_classes = [IsLoggedInUserOrAdmin]
+        if self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
