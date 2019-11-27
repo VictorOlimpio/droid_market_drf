009fd7df2f04911df6f2497b3957f51e16c67bfc
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-26 21:17:06 -0300

    feature: altera campo type de pieces

--- pieces/models.py
+++ pieces/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+TYPES = (
+    ("arm", "Arm"),
+    ("leg", "Leg"),
+    ("body", "Body"),
+    ("head", "Head"),
+    ("blaster", "Blaster"),
+    ("shield", "Shield")
+)
+
+
 class Piece(models.Model):
-    type = models.IntegerField()
+    type = models.CharField(max_length=15, choices= TYPES)
     description = models.TextField(max_length=400)
     value = models.FloatField()
