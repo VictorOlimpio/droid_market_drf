4fb4b1eb4816be9bf1c1e48f43891eab525aab1d
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-26 23:19:34 -0300

    feature: adiciona modelo de demandas

--- demands/models.py
+++ demands/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import UserProfile
+from pieces.models import Piece
 
-# Create your models here.
+STATUSES = (
+    (0, "Opened"),
+    (1, "Finnished")
+)
+
+class Demand(models.Model):
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="demands")
+    piece = models.ForeignKey(Piece, on_delete=models.CASCADE, related_name="demands")
+    street = models.CharField(max_length=50)
+    neighborhood = models.CharField(max_length=20)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=20)
+    number = models.IntegerField()
+    cep = models.CharField(max_length=9)
+    status = models.IntegerField(default=0, choices= STATUSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
