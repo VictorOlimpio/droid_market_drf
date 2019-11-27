864e2187ee730560aa5b5dbc1c1aa5eecbc73e9e
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-27 10:21:24 -0300

    fix: corrige erro de não geração de token para superuser

--- droid_market/settings.py
+++ droid_market/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ],
+    )
 }
