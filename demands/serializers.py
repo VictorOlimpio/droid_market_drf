eb96179a723c4f630e02b1b362f5d4c7730ccf13
Victor Olimpio <victorolimpio@MacBook-Pro-de-Victor.local>
2019-11-27 21:30:10 -0300

    feature: adiciona hyperlink entre as relações

--- demands/serializers.py
+++ demands/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from demands.models import Demand
 
-class DemandSerializer(serializers.ModelSerializer):
+class DemandSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Demand
         fields = "__all__"
